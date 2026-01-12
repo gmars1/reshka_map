a828691d5be99f213187820b6b76cf783350b92c
Marsel Gazizov <112752635+gmars1@users.noreply.github.com>
2026-01-12 17:02:07 +0300

    move to dir
    update

---
+++ js/cache.js
@@ -0,0 +1,15 @@
+
+
+const cache = localStorage;
+
+export function addGeoCache(key, value) {
+    cache.setItem(key, JSON.stringify(value));
+}
+
+export function getGeoCache(key) {
+    const cached = cache.getItem(key)
+    if(cached) return JSON.parse(cached);
+
+    return null;
+}
+
